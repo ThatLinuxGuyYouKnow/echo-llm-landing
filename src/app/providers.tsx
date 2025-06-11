5732499a21a3da3352ce9498eee0cb83db1dd72d
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-11 23:34:50 +0100

    chore: using localstorage for persistence rather thans etting cookies

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -11,9 +11,9 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-
             api_host: '/ingest',
-            person_profiles: 'always', capture_pageview: false // Disable automatic pageview capture, as we capture manually
+            capture_pageview: false,
+            persistence: 'localStorage',
         })
     }, [])
 
