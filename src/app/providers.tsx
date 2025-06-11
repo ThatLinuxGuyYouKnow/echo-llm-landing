13c7936a6c78d10317069b85c9b313a6e1faeb9f
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-11 22:50:14 +0100

    chore: changed proxy url to explicit url

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -12,7 +12,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
 
-            api_host: '/ingest',
+            api_host: 'https://echo-llm-hazel.vercel.app/ingest',
             person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
             capture_pageview: false // Disable automatic pageview capture, as we capture manually
         })
