d263bf8326286a5e513d5af986f55ac23860eaad
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-13 14:00:00 +0100

    chore: mod on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { PostHogProvider } from "./providers";
+
+
+import './globals.css'
+import { PostHogProvider } from './providers'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -11,25 +14,18 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "EchoLLM",
   description: "The latest and Greatest AI models, all in one place",
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <PostHogProvider>  {children}</PostHogProvider>
-
+      <body>
+        <PostHogProvider>
+          {children}
+        </PostHogProvider>
       </body>
     </html>
-  );
+  )
 }
