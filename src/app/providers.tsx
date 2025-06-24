68288c51b1276e20ecda75a03362deb0160374df
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-25 00:01:49 +0100

    feature: recording profile for anonymous users

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -12,7 +12,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
             api_host: '/ingest',
-            person_profiles: 'identified_only',
+            person_profiles: 'always',
             capture_pageview: false
         })
     }, [])
